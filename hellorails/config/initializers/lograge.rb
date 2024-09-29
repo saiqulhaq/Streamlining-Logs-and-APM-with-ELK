e607e80ec71804174194ef767945ad3cb6aa83ac
Saiqul Haq <saiqulhaq@gmail.com>
2024-09-29 12:34:50 +0700

    add APM and kibana key

--- hellorails/config/initializers/lograge.rb
+++ hellorails/config/initializers/lograge.rb
@@ -1,15 +1,21 @@
 Rails.application.configure do
+  config.log_tags = [ :request_id, proc { ElasticAPM.log_ids } ]
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::Logstash.new
   config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/application_server.log"
   config.lograge.custom_options = lambda do |event|
     exceptions = %w(controller action format id)
-    {
-      level: event.payload[:level],
-      request_id: event.payload[:request_id] || Current.request_id,
-      ip: event.payload[:ip],
-      params: event.payload[:params].except(*exceptions)
-    }
+    ElasticAPM.log_ids do |transaction_id, span_id, trace_id|
+      {
+        level: event.payload[:level],
+        request_id: event.payload[:request_id] || Current.request_id,
+        ip: event.payload[:ip],
+        params: event.payload[:params].except(*exceptions),
+        transaction_id: transaction_id,
+        span_id: span_id,
+        trace_id: trace_id
+      }
+    end
   end
 
   config.lograge.custom_payload do |controller|
